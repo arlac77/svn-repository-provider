de688e06aa0d96ff00f2e0c6af3fcb3ae4ef756f
Markus Felten <Markus.Felten@gmx.de>
2018-01-24 20:29:39 +0100

    feat: skeleton implementation

--- src/svn-repository-provider.js
+++ src/svn-repository-provider.js
@@ -4,12 +4,26 @@ import { Provider, Repository, Branch } from 'repository-provider';
  * Provider for subversion repositories
  */
 export class SubversionProvider extends Provider {
+  /**
+   * @return {Class} SubversionRepository
+   */
   get repositoryClass() {
     return SubversionRepository;
   }
 
+  /**
+   * @return {Class} SubversionBranch
+   */
   get branchClass() {
     return SubversionBranch;
+  }
+
+  /**
+   * Deliver the repository type
+   * @return {string} 'svn'
+   */
+  get type() {
+    return 'svn';
   }
 }
 
