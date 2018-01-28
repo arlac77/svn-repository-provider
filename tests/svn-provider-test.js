6a3b2385a98cc2201e3a4c8a98b3094b19fbd738
Markus Felten <Markus.Felten@gmx.de>
2018-01-28 13:33:39 +0100

    test: preps for some real tests

--- tests/svn-provider-test.js
+++ tests/svn-provider-test.js
@@ -1,8 +1,31 @@
 import test from 'ava';
 import { SubversionProvider } from '../src/svn-repository-provider';
 
-test('svn provider', async t => {
+const credentials = {
+  password: 'xxx',
+  user: 'yyy'
+};
+
+if (process.env.SVN_USER) {
+  credentials.user = process.env.SVN_USER;
+}
+
+if (process.env.SVN_PASSWORD) {
+  credentials.password = process.env.SVN_PASSWORD;
+}
+
+test('svn provider type', async t => {
   const provider = new SubversionProvider();
 
   t.is(provider.type, 'svn');
+});
+
+test.skip('svn provider', async t => {
+  const provider = new SubversionProvider(credentials);
+
+  const repository = await provider.repository(
+    'https://subversion.assembla.com/svn/delivery_notes'
+  );
+
+  t.is(repository.name, 'delivery_notes');
 });
