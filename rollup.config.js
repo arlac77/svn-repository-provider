fe7b41adb600650d89a8e2a4b1e56858b194181d
Markus Felten <Markus.Felten@gmx.de>
2018-09-26 02:33:27 +0200

    fix(package): sync from template

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-import cleanup from 'rollup-plugin-cleanup';
-import executable from 'rollup-plugin-executable';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import pkg from './package.json';
+import cleanup from "rollup-plugin-cleanup";
+import executable from "rollup-plugin-executable";
+import commonjs from "rollup-plugin-commonjs";
+import resolve from "rollup-plugin-node-resolve";
+import pkg from "./package.json";
 
 export default {
   input: pkg.module,
   output: {
     file: pkg.main,
-    format: 'cjs'
+    format: "cjs",
+    interop: false
   },
-  external: ['repository-provider'],
-  plugins: []
+  external: ["repository-provider"],
+  plugins: [resolve(), commonjs(), cleanup()]
 };
